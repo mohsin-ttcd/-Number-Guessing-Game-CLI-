bf1a942b71e7306f6cdd7c6ad02ad776770df7cf
agentMohsin <mohsin.ttcdl@gmail.com>
2026-05-03 17:14:34 +0600

    Use function to reuse guessing game any where and anytime throughout codebase, also add a feature to asks the player if they want to restart or end

--- main.py
+++ main.py
@@ -3,38 +3,63 @@
 #import random module
 import random
 
-target_number = random.randint(0,20)
-max_guess_limit = 5
+#wrape number guessing game inside a function
+def play_number_guessing_game():
 
-print(f"***** Game Start *****")
-print(f"*** Max guess limit {max_guess_limit} ***")
-print(f"Enter number between 0 - 20\n")
+    #target and max guess limit
+    target_number = random.randint(0,20)
+    max_guess_limit = 5
 
-while max_guess_limit > 0:
 
-    try:
-        user_input = int(input("Guess The Number: "))
-    
-    except ValueError:
-        print("Please enter a integer number between 0 - 20")
-        continue
+    print(f"***** Game Start *****")
+    print(f"*** Max guess limit {max_guess_limit} ***")
+    print(f"Enter number between 0 - 20\n")
 
-    if user_input == target_number:
-        print(f"\n🥇 You won the game!")
-        print(f"🎯 Target number was {target_number}")
-        print(f"🎮 You enter {user_input}\n")
+    while max_guess_limit > 0:
+
+        # Input validation
+        try:
+            user_input = int(input("Guess The Number: "))
+        
+        except ValueError:
+            print("\n❌ Please enter a integer number between 0 - 20\n")
+            continue
+
+
+        # Game engine logic
+        if user_input == target_number:
+            print(f"\n🥇 You won the game!")
+            print(f"🎯 Target number was {target_number}")
+            print(f"🎮 You enter: {user_input}\n")
+            break
+        
+        elif user_input > target_number:
+            print(f"\n❌ Too high!")
+            print("🔄️ Try again!\n")
+
+        else:
+            print("\n❌ Too low!")
+            print("🔄️ Try again!\n")
+
+        max_guess_limit = max_guess_limit - 1
+
+    # Game over check
+    if max_guess_limit == 0:
+        print("\n❌ Game Over!")
+        print("❌ You hit the max limit!\n")
+        print(f"🎯 Target limit was {max_guess_limit}")
+
+
+# Main loop 
+# Run the game
+while True:
+    play_number_guessing_game()
+
+    replay = input(f"Did you want to play again? y/n: ").lower()
+
+    if replay == "n":
+        print(f"\n🥳 Game End!\n")
         break
-    
-    elif user_input > target_number:
-        print(f"\n❌ Too high!")
-        print("🔄️ Try again!\n")
-
+        
     else:
-        print("\n❌ Too low!")
-        print("🔄️ Try again!\n")
-
-    max_guess_limit = max_guess_limit - 1
-
-if max_guess_limit == 0:
-    print("\n❌ Game Over!")
-    print("❌ You hit the max limit!\n")
+        continue
