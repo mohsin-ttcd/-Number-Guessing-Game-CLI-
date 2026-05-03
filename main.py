c4632146763aa52dbc5b2a5bd6ca8afab7f905a8
agentMohsin <mohsin.ttcdl@gmail.com>
2026-05-03 16:33:47 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# number guessing game
+
+#import random module
+import random
+
+target_number = random.randint(0,20)
+max_guess_limit = 5
+
+print(f"***** Game Start *****")
+print(f"*** Max guess limit {max_guess_limit} ***")
+print(f"Enter number between 0 - 20\n")
+
+while max_guess_limit > 0:
+
+    user_input = int(input("Guess The Number: "))
+
+    if user_input == target_number:
+        print(f"\n🥇 You won the game!")
+        print(f"🎯 Target number was {target_number}")
+        print(f"🎮 You enter {user_input}\n")
+        break
+    
+    elif user_input > target_number:
+        print(f"\n❌ Too high!")
+        print("🔄️ Try again!\n")
+
+    else:
+        print("\n❌ Too low!")
+        print("🔄️ Try again!\n")
+
+    max_guess_limit = max_guess_limit - 1
+
+if max_guess_limit == 0:
+    print("\n❌ Game Over!")
+    print("❌ You hit the max limit!\n")
