b25ea4f7e662cdd87ff60025493f3243e3723c47
agentMohsin <mohsin.ttcdl@gmail.com>
2026-05-04 23:19:50 +0600

    Now the game can able to record the lost attemp take to achive the goal. also update the random generation number dynamicly outside from the fuction

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 import random
 
 # Number guessing game inside a function
-def play_number_guessing_game():
+def play_number_guessing_game(max_guess_limit, target_between):
 
-    target_number = random.randint(0,20)
-    max_guess_limit = 5
+    target_number = random.randint(0,target_between)
     attempt = 1 
     win = 0
     lose = 0
@@ -13,19 +12,19 @@ def play_number_guessing_game():
 
     print(f"\n***** Game Start *****")
     print(f"*** Max guess limit {max_guess_limit} ***")
-    print(f"Enter number between 0 - 20\n")
+    print(f"Enter number between 0 - {target_between}\n")
 
     while max_guess_limit > 0:
 
         # Count remaining attempt
         attempt_left = max_guess_limit - 1
-                
+        
         # Input validation
         try:
             user_input = int(input("Guess The Number: "))
         
         except ValueError:
-            print("\n❌ Please enter a integer number between 0 - 20\n")
+            print(f"\n❌ Please enter a integer number between 0 - {target_between}\n")
             continue
 
         # Game engine logic
@@ -55,41 +54,56 @@ def play_number_guessing_game():
         print(f"🎯 Target number was {target_number}\n")
         lose = 1
 
-    return win, lose, play
+    return win, lose, play, attempt
 
+target_between = 20
+max_guess_limit = 5
 total_win = 0
 total_lose = 0
 game_play = 0
+best_score = []
 
 # Main loop 
 # Run the game
 while True:
 
-    win, lose, play = play_number_guessing_game()
+    win, lose, play, attempt= play_number_guessing_game(max_guess_limit,target_between)
     total_win += win
     total_lose += lose
     game_play += play
+    
+    # New feature to add 
+    # tracking the fewest number of attempts it took to win
+    if win == 1:
+        if best_score == []:
+            best_score.append(attempt)
+        
+        else:
+            for i in best_score:
+                if i > attempt:
+                    best_score = [attempt]
 
-
+    # Replay input validation
     while True:
 
-        replay = input(f"Do you want to play again? y/n: ").lower()
+        replay = input(f"Do you want to play again? y/n: ").strip().lower()
 
         if replay == "y" or replay == "n":
             break
         else:
-            print(f"\n❌ Invalid input! Please enter 'y' for yes and 'n' for no.")
+            print(f"\n❌ Invalid input! Please enter 'y' for yes and 'n' for no.\n")
 
     
     if replay == "n":
         print(f"\n🥳 Game End!\n")
-        print(f"▶️ You played {game_play} round.")
+        if len(best_score) > 0:
+            print(f"🎉 New Record {best_score[0]}")
+        else:
+            print(f"No wins yet")
+        print(f"▶️  You played {game_play} round.")
         print(f"🏆 Won {total_win} & 😔 Lost {total_lose}.")
         break
         
     else:
         continue
 
-
-# New feature to add 
-# tracking the fewest number of attempts it took to win
